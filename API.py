7fc47f350da001d4ea534587f9fa194adfcdebd8
Jonathan Juhas <ignus.gracia@gmail.com>
2016-09-06 11:08:36 +0200

    Fix incorrect service name

--- API.py
+++ API.py
@@ -2,13 +2,13 @@ from lxml import html
 import requests
 import sys
 
-class ColorizerAPI:
+class ColorizeAPI:
 
-    colorizerURL = "https://alexbeals.com/projects/colorize/search.php?q="
+    colorizeURL = "https://alexbeals.com/projects/colorize/search.php?q="
 
     @staticmethod
     def query(query):
-        page = requests.get(ColorizerAPI.colorizerURL + query)
+        page = requests.get(ColorizeAPI.colorizeURL + query)
         tree = html.fromstring(page.content)
 
         color_code = tree.xpath('//span[@class="hex"]/text()')[0]
@@ -18,4 +18,4 @@ class ColorizerAPI:
 if len(sys.argv) != 2:
     print "Please provide the query parameter"
 else:
-    ColorizerAPI.query(sys.argv[1])
+    ColorizeAPI.query(sys.argv[1])
